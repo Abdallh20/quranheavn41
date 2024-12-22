75c88f3c83381feebf1bde162f256386076d3a68
Abdallh20 <abdallhshref4@gmail.com>
2024-12-23 01:03:34 +0200

    dwsdw

--- quranheaven/settings.py
+++ quranheaven/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'allauth.account.middleware.AccountMiddleware',
 ]
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
