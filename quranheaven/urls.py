dd65399e131779f670ed71b471a7645a799e5df9
Abdallh20 <abdallhshref4@gmail.com>
2024-12-24 02:41:02 +0200

    dwsdw

--- quranheaven/urls.py
+++ quranheaven/urls.py
@@ -54,8 +54,5 @@ urlpatterns = [
 urlpatterns += [
     path('submit_fatwa/', submit_fatwa, name='submit_fatwa'),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
